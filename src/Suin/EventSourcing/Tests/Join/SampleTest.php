c47fe143e9fb403212380592393e858c9e9c48f5
suin <suinyeze@gmail.com>
2013-05-06 20:00:09 +0900

    set up project

---
+++ src/Suin/EventSourcing/Tests/Join/SampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Suin\EventSourcing\Tests\Join;
+
+class SampleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSample()
+    {
+        $this->assertTrue(false);
+    }
+}
